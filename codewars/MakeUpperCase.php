2a084a258104f567660f72ddcdf60846251fa26a
hamle <hamletcruzpirazan@gmail.com>
2023-08-17 19:14:50 -0500

    CodeWars-MakeUpperCase

---
+++ codewars/MakeUpperCase.php
@@ -0,0 +1,8 @@
+function makeUpperCase($inputString) {
+    return strtoupper($inputString);
+}
+
+// Example usage
+$input = "Hello, World!";
+$output = makeUpperCase($input);
+echo $output; // Output: "HELLO, WORLD!"
